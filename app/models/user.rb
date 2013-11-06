f9a0cb2478d45540b81d49b1f2b70cbd1632f28e
Darren Doyle <darsedoyle@gmail.com>
2013-11-06 13:38:33 +0000

    having errors need looking,got the finshed sample

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,17 @@
 class User < ActiveRecord::Base
-	attr_accessor :name, :email
+	
+	attr_accessor :name, :email, :password, :password_confirmation
+	
+	#has_secure_password
+
+	before_save {|user| user.email= user.downcase}
+
+	validates :name, presence:true, length:{maximum:50}
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, 
+  						uniqueness: { case_sensitive: false }
+  	validates :password, length:{minimum:6}
+  
+
 end
+  	
