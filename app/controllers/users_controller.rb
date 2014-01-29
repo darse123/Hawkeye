eb87b5a28284c0d2efa0a4e75b018cfffafdf50a
Darren Doyle <darsedoyle@gmail.com>
2014-01-29 20:59:31 +0000

    dropmenu working now 9.20

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,27 @@
 class UsersController < ApplicationController
-  	def show
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+       flash[:success] = "Profile updated"
+       redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
+  def show
   		@user = User.find(params[:id])
-  	
- 	end
+  end
 	def new
   		@user = User.new
-  	end
-  	def create
+  end
+
+  def create
   		 @user = User.new(user_params)
   		if @user.save
   			  flash[:success] = "Welcome to the Sample App!"
@@ -15,13 +30,12 @@ class UsersController < ApplicationController
   		else
   			render 'new'
   		end
-  	end
+  end
 
  	private
-
-   	def user_params
+  def user_params
       	params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
-   	 	end
-	end
+  end
+end
 
