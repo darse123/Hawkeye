070a980c3edc9511a3e3baf1b4d15dfdc527642f
Darren Doyle <darsedoyle@gmail.com>
2014-02-12 14:32:02 +0000

    problem 9.2.2 now passing..next stage Friendly forwarding

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -8,9 +8,14 @@ module SessionsHelper
  	def signed_in?
     	!current_user.nil?
     end
+    
     def current_user=(user)
     @current_user = user
- 	end
+ 	  end
+ 	  
+ 	  def current_user?(user)
+      user == current_user
+    end
     def current_user
     	remember_token = User.encrypt(cookies[:remember_token])
     	@current_user ||= User.find_by(remember_token: remember_token)
