cbb723b6995ff4da3b4a9b2f36bca603d189aeaa
Darren Doylee <darsedoyle@gmail.comm>
2014-03-13 20:53:49 +0000

    Add user following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -51,17 +51,16 @@ class UsersController < ApplicationController
 	def new
   		@user = User.new
   end
-
-  def create
-  		 @user = User.new(user_params)
-  		if @user.save
-  			  flash[:success] = "Welcome to the Sample App!"
-  			redirect_to @user
-  		# Handle a successful save
-  		else
-  			render 'new'
-  		end
-  end
+   def create
+      @user = User.new(user_params)
+      if @user.save
+        sign_in @user
+        flash[:success] = "Welcome to the Sample App!"
+        redirect_to @user
+      else
+        render 'new'
+      end
+    end
 
  	 private
 
@@ -69,12 +68,7 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
-  def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in."
-      end
-   end
+  
   def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
